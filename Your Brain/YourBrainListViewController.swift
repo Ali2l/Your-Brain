a65290a0902a2cf38ba40b5cd278cea8062fd2ba
Ali <ali_y_k@hotmail.com>
2019-03-02 00:42:04 +0300

    Adding Table View and make prototype list with checkmarks.

---
+++ Your Brain/YourBrainListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Your Brain
+//
+//  Created by Ali Al-khowaiter on 3/1/19.
+//  Copyright © 2019 Ali Al-khowaiter. All rights reserved.
+//
+
+import UIKit
+
+class YourBrainListViewController: UITableViewController {
+    
+    let itemArray = ["Find Ali", "Work on App" , "Shower"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "YourBrainItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // print(itemArray[indexPath.row])
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
