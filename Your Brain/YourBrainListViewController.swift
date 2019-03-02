cba123ddba1556cc7fb01e16ac8628113d35e74e
Ali <ali_y_k@hotmail.com>
2019-03-02 14:04:30 +0300

    adding and using UserDefult Method.

--- Your Brain/YourBrainListViewController.swift
+++ Your Brain/YourBrainListViewController.swift
@@ -11,33 +11,40 @@ import UIKit
 class YourBrainListViewController: UITableViewController {
     
     var itemArray = ["Find Ali", "Work on App" , "Shower"]
+    let defults = UserDefaults.standard
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
+    
+    
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "YourBrainItemCell", for: indexPath)
-        
         cell.textLabel?.text = itemArray[indexPath.row]
-        
         return cell
     }
+    
+    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // print(itemArray[indexPath.row])
-        
-        
-        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
             
         } else {
@@ -47,27 +54,27 @@ class YourBrainListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Add New To DO Item", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //what will happen when user clicks the add item buttun on UIAlert
             
+            //what will happen when user clicks the add item buttun on UIAlert
             self.itemArray.append(textField.text!)
+            self.defults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
-    
             textField = alertTextField
         }
         
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
     }
     
